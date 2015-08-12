48f24e6c8e7829952dd58e9f783a8674e4660aff
MATSUS <susmateusz@gmail.com>
2015-08-12 15:58:02 +0200

    Added LibraryEntity with source

---
+++ spring-data-model/src/main/java/pl/spring/demo/entity/LibraryEntity.java
@@ -0,0 +1,54 @@
+package pl.spring.demo.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LIBRARY")
+public class LibraryEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(nullable = false, length = 50)
+	private String name;
+	@OneToMany
+	@JoinTable(name = "LIBRARY_BOOK", joinColumns = @JoinColumn(name = "library_id") , inverseJoinColumns = @JoinColumn(name = "book_id") )
+	private Set<BookEntity> books = new HashSet<>();
+
+	public LibraryEntity() {
+	}
+
+	public LibraryEntity(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
