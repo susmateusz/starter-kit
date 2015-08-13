c3c095df6e488af065936aba3e8d92b39c5405f8
MATSUS <susmateusz@gmail.com>
2015-08-13 15:27:12 +0200

    Added transactional: readOnly=false to remove method

--- spring-data-jpa/src/main/java/pl/spring/demo/service/impl/LibraryServiceImpl.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/impl/LibraryServiceImpl.java
@@ -2,18 +2,21 @@ package pl.spring.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import pl.spring.demo.mapper.BookMapper;
 import pl.spring.demo.repository.LibraryRepository;
 import pl.spring.demo.service.LibraryService;
 
 @Service
+@Transactional(readOnly = true)
 public class LibraryServiceImpl implements LibraryService {
 
 	@Autowired
 	private LibraryRepository LibraryRepository;
 
 	@Override
+	@Transactional(readOnly = false )
 	public void removeLibraryById(long id) {
 		LibraryRepository.delete(id);
 	}
