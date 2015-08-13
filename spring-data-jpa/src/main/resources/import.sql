803b45a5a9f2fe66d5a23a6c026496b4f4fd060a
MATSUS <susmateusz@gmail.com>
2015-08-13 14:39:14 +0200

    Added records to import.sql and removed unnecessary library_book table

--- spring-data-jpa/src/main/resources/import.sql
+++ spring-data-jpa/src/main/resources/import.sql
@@ -1,6 +1,11 @@
-insert into book (id, title) values (1, 'Pierwsza książka');
-insert into book (id, title) values (2, 'Druga książka');
-insert into book (id, title) values (3, 'Trzecia książka');
+
+insert into library (id, name) values (13, 'MBP filia nr 1');
+insert into library (id, name) values (14, 'MBP filia nr 2');
+insert into library (id, name) values (15, 'Biblioteka PWr filia nr 1');
+
+insert into book (id, title, library_id) values (1, 'Pierwsza książka',13);
+insert into book (id, title, library_id) values (2, 'Druga książka',13);
+insert into book (id, title, library_id) values (3, 'Trzecia książka',14);
 
 insert into author (id, first_name, last_name) values (7, 'Jan', 'Kowalski');
 insert into author (id, first_name, last_name) values (8, 'Zbigniew', 'Nowak');
