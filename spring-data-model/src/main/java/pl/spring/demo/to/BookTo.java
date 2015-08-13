2b426086bb8acff7b64ee1b065578145ffe49612
MATSUS <susmateusz@gmail.com>
2015-08-13 14:42:11 +0200

    Added equals() to BookTo, libraryDao bean to CommonDaoTest-context and commented out unnecessary mapper bean.

--- spring-data-model/src/main/java/pl/spring/demo/to/BookTo.java
+++ spring-data-model/src/main/java/pl/spring/demo/to/BookTo.java
@@ -1,40 +1,54 @@
 package pl.spring.demo.to;
 
+import java.util.Objects;
+
 public class BookTo {
-    private Long id;
-    private String title;
-    private String authors;
+	private Long id;
+	private String title;
+	private String authors;
 
-    public BookTo() {
-    }
+	public BookTo() {
+	}
 
-    public BookTo(Long id, String title, String authors) {
-        this.id = id;
-        this.title = title;
-        this.authors = authors;
-    }
+	public BookTo(Long id, String title, String authors) {
+		this.id = id;
+		this.title = title;
+		this.authors = authors;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-    public String getAuthors() {
-        return authors;
-    }
+	public String getAuthors() {
+		return authors;
+	}
 
-    public void setAuthors(String authors) {
-        this.authors = authors;
-    }
+	public void setAuthors(String authors) {
+		this.authors = authors;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final BookTo other = (BookTo) obj;
+		return Objects.equals(this.getAuthors(), other.getAuthors())
+				&& Objects.equals(this.getTitle(), other.getTitle()) && Objects.equals(this.getId(), other.getId());
+	}
+
 }
