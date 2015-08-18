df1274d4326c847a625b083f6017e66ec892d154
MATSUS <susmateusz@gmail.com>
2015-08-18 09:53:21 +0200

    libraryName field to BookTo

--- spring-data-model/src/main/java/pl/spring/demo/to/BookTo.java
+++ spring-data-model/src/main/java/pl/spring/demo/to/BookTo.java
@@ -6,14 +6,16 @@ public class BookTo {
 	private Long id;
 	private String title;
 	private String authors;
+	private String libraryName;
 
 	public BookTo() {
 	}
 
-	public BookTo(Long id, String title, String authors) {
+	public BookTo(Long id, String title, String authors, String libraryName) {
 		this.id = id;
 		this.title = title;
 		this.authors = authors;
+		this.libraryName = libraryName;
 	}
 
 	public Long getId() {
@@ -38,6 +40,14 @@ public class BookTo {
 
 	public void setAuthors(String authors) {
 		this.authors = authors;
+	}
+	
+	public String getLibraryName() {
+		return libraryName;
+	}
+	
+	public void setLibraryName(String libraryName) {
+		this.libraryName = libraryName;
 	}
 
 	@Override
