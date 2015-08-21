26c3e1869b4706143cf77a5cfc0d2aae1a55f7c5
MATSUS <susmateusz@gmail.com>
2015-08-21 15:28:23 +0200

    AddBook modal functionality implemented without sending book and closing modal.

--- web/src/main/client/app/books/js/add-book.controller.js
+++ web/src/main/client/app/books/js/add-book.controller.js
@@ -1,8 +1,15 @@
-angular.module('app.books').controller('AddBookController', function ($scope, $window, $location, bookService, Flash) {
-    'use strict';
+angular.module('app.books').controller(
+		'AddBookController',
+		function($scope, $window, $location, bookService, Flash, $modal) {
+			'use strict';
+			
+			$scope.title = '';
 
-    $scope.addBook = function (bookTitle,authorFirstName,authorLastName) {
-            Flash.create('success', 'Książka "'+bookTitle+'"została dodana!', 'custom-class');
-    };
+			$scope.addBook = function(bookTitle, authorFirstName,
+					authorLastName) {
 
-});
+				Flash.create('success', 'Książka "' + bookTitle
+						+ '"została dodana!', 'custom-class');
+			};
+
+		});
--- web/src/main/client/app/books/js/book-modal.controller.js
+++ web/src/main/client/app/books/js/book-modal.controller.js
@@ -1,5 +1,38 @@
-angular.module('app.books').controller('BookModalController', function ($scope) {
-    'use strict';
+angular.module('app.books').controller(
+		'BookModalController',
+		function($scope, $modal, Flash) {
+			'use strict';
 
-    $scope.title = 'title';
-});
+			$scope.bookTitle = '';
+			$scope.authors = [];
+			$scope.index = 0;
+			$scope.firstName = '';
+			$scope.lastName = '';
+			$scope.gridOptions = {
+				data : 'authors'
+			};
+
+			$scope.addBook = function() {
+				$close();
+				var book ={title : $scope.bookTitle, authors : $scope.authors};
+				bookService.addBook($book).then(function(){
+					Flash.create('success', 'Książka '+$book+' została dodana!', 'custom-class');
+				}, function(){
+					Flash.create('danger', 'Wystapił problem  podczas dodawania książki!', 'custom-class');
+				});
+			};
+
+			$scope.addAuthor = function(authorFirstName, authorLastName) {
+				$scope.index = $scope.authors.length;
+				$scope.authors[$scope.index] = {
+					firstName : authorFirstName,
+					lastName : authorLastName
+				};
+				$scope.firstName = '';
+				$scope.lastName = '';
+			};
+
+			$scope.deleteAuthor = function(authorIndex) {
+				$scope.authors.splice(authorIndex, 1);
+			};
+		});
