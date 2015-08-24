f2123a6a83bbab5a9e5971d2b40a2ab4e243de02
MATSUS <susmateusz@gmail.com>
2015-08-24 15:49:08 +0200

    Changed format of authors from BookTo to AuthorTo in tests.

--- web/src/test/java/pl/spring/demo/web/rest/BookRestServiceTest.java
+++ web/src/test/java/pl/spring/demo/web/rest/BookRestServiceTest.java
@@ -1,5 +1,15 @@
 package pl.spring.demo.web.rest;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,17 +23,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
 import pl.spring.demo.service.BookService;
+import pl.spring.demo.to.AuthorTo;
 import pl.spring.demo.to.BookTo;
 import pl.spring.demo.web.utils.FileUtils;
-
-import java.io.File;
-import java.util.Arrays;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
@@ -47,9 +51,13 @@ public class BookRestServiceTest {
     public void testShouldCallBookService() throws Exception {
         // given
         final String bookTitle = "testTitle";
-
-        final BookTo bookTo1 = new BookTo(1L, bookTitle, "Author1");
-        final BookTo bookTo2 = new BookTo(2L, bookTitle, "Author2");
+        final AuthorTo authorTo1 = new AuthorTo(1L, "name1", "last name 1");
+        final AuthorTo authorTo2 = new AuthorTo(1L, "name2", "last name 2");
+        Set<AuthorTo> authors = new HashSet<AuthorTo>();
+        authors.add(authorTo1);
+        authors.add(authorTo2);
+        final BookTo bookTo1 = new BookTo(1L, bookTitle, authors);
+        final BookTo bookTo2 = new BookTo(2L, bookTitle, authors);
 
         Mockito.when(bookService.findBooksByTitle(bookTitle)).thenReturn(Arrays.asList(bookTo1, bookTo2));
 
@@ -64,11 +72,11 @@ public class BookRestServiceTest {
 
                 .andExpect(jsonPath("[0].id").value(bookTo1.getId().intValue()))
                 .andExpect(jsonPath("[0].title").value(bookTo1.getTitle()))
-                .andExpect(jsonPath("[0].authors").value(bookTo1.getAuthors()))
+                .andExpect(jsonPath("[0].authors[0].firstName").value(authorTo1.getFirstName()))
 
                 .andExpect(jsonPath("[1].id").value(bookTo2.getId().intValue()))
                 .andExpect(jsonPath("[1].title").value(bookTo2.getTitle()))
-                .andExpect(jsonPath("[1].authors").value(bookTo2.getAuthors()));
+                .andExpect(jsonPath("[1].authors[0].firstName").value(authorTo1.getFirstName()));
     }
 
     @Test
