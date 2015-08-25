10e0aaac5181973f1d478fba64a8a21cafe7695c
MATSUS <susmateusz@gmail.com>
2015-08-25 12:19:26 +0200

    Added authors module to configs of modules.

--- web/src/main/client/app/app.module.js
+++ web/src/main/client/app/app.module.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute',  'app.main', 'app.component1', 'app.component2', 'app.books', 'flash'])
+angular.module('app', ['ngRoute', 'app.authors','app.main', 'app.component1', 'app.component2', 'app.books', 'flash'])
     .config(function ($locationProvider) {
         'use strict';
         $locationProvider.html5Mode(false);
