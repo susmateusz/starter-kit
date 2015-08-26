7aeec2c580f7daf7e2a6976454f69c90193d913c
MATSUS <susmateusz@gmail.com>
2015-08-26 12:38:31 +0200

    Changes to stage

---
+++ web/src/main/client/app/books/js/book.search.service.spec.js
@@ -0,0 +1,70 @@
+describe('book  search service', function() {
+	'use strict';
+
+	beforeEach(function() {
+		module('app.main');
+		module('app.books');
+	});
+
+	var $scope;
+	beforeEach(inject(function($rootScope) {
+		$scope = $rootScope.$new();
+	}));
+
+	it('search is defined', inject(function(bookService) {
+		// then
+		expect(bookService.search).toBeDefined();
+	}));
+
+	it('search book should call bookService.search', inject(function(
+			bookService, bookRestService) {
+		// given
+		var prefix = '';
+		spyOn(bookRestService, 'search').and.returnValue();
+		// when
+		bookService.search(prefix);
+		// then
+		expect(bookRestService.search).toHaveBeenCalledWith(prefix);
+	}));
+
+	it('deleteBook is defined', inject(function(bookService) {
+		// then
+		expect(bookService.deleteBook).toBeDefined();
+	}));
+
+	it('deleteBook should call bookService.deleteBook', inject(function(
+			bookService, bookRestService) {
+		// given
+		var bookId = 3;
+		spyOn(bookRestService, 'deleteBook').and.returnValue();
+		// when
+		bookService.deleteBook(bookId);
+		// then
+		expect(bookRestService.deleteBook).toHaveBeenCalledWith(bookId);
+	}));
+
+	it('addBook is defined', inject(function(bookService) {
+		// then
+		expect(bookService.addBook).toBeDefined();
+	}));
+
+	it('addBook should call bookService.addBook', inject(function(bookService,
+			bookRestService) {
+		// given
+		var book = {
+			'id' : 1,
+			'title' : 'title',
+			'authors' : [ {
+				'id' : 7,
+				'firstName' : 'Jan',
+				'lastName' : 'Kowalski'
+			} ]
+		};
+		spyOn(bookRestService, 'addBook').and.returnValue();
+		// when
+		bookService.addBook(book);
+		// then
+		expect(bookRestService.addBook).toHaveBeenCalledWith(book);
+	}));
+
+});
