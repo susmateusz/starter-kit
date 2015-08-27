4d49cdba1423f20969bb86c82f8551e0af12adb5
MATSUS <susmateusz@gmail.com>
2015-08-27 12:53:36 +0200

    Working tests for book-search.controller. Added Flashes when warnings.

--- web/src/main/client/app/books/js/book.search.controller.spec.js
+++ web/src/main/client/app/books/js/book.search.controller.spec.js
@@ -14,28 +14,22 @@ describe('book controller', function() {
 	beforeEach(inject(function($rootScope, $controller) {
 		$scope = $rootScope.$new();
 		modalInstance = {
-			close : jasmine.createSpy('modalInstance.close'),
-			dismiss : jasmine.createSpy('modalInstance.dismiss'),
+			close : jasmine.createSpy('modalInstance.close'), dismiss : jasmine.createSpy('modalInstance.dismiss'),
 			// result : defferedPromise
 			result : {
 				then : jasmine.createSpy('modalInstance.result.then')
 			}
 		};
 		var exampleBooks = [ {
-			'id' : 1,
-			'title' : 'title',
-			'authors' : [ {
-				'id' : 7,
-				'firstName' : 'Jan',
-				'lastName' : 'Kowalski'
+			'id' : 1, 'title' : 'title', 'authors' : [ {
+				'id' : 7, 'firstName' : 'Jan', 'lastName' : 'Kowalski'
 			} ]
 		} ];
 		ctrl = $controller('BookSearchController', {
-			$scope : $scope,
-			books : exampleBooks,
+			$scope : $scope, books : exampleBooks,
 		});
 	}));
-	
+
 	it('filter should detect right title', inject(function() {
 		// given
 		var title = 'Pierwsza ksiazka';
@@ -45,7 +39,7 @@ describe('book controller', function() {
 		// then
 		expect(result).toBeTruthy();
 	}));
-	
+
 	it('filter should detect any book with prefix', inject(function() {
 		// given
 		var title = 'Pierwsza ksiazka';
@@ -61,17 +55,12 @@ describe('book controller', function() {
 		expect($scope.search).toBeDefined();
 	}));
 
-	it('search book should call bookService.search', inject(function($q,
-			bookService) {
+	it('search book should call bookService.search', inject(function($q, bookService) {
 		// given
 		var result = {
 			'data' : [ {
-				'id' : 1,
-				'title' : 'title',
-				'authors' : [ {
-					'id' : 7,
-					'firstName' : 'Jan',
-					'lastName' : 'Kowalski'
+				'id' : 1, 'title' : 'title', 'authors' : [ {
+					'id' : 7, 'firstName' : 'Jan', 'lastName' : 'Kowalski'
 				} ]
 			} ]
 		};
@@ -89,8 +78,7 @@ describe('book controller', function() {
 		expect($scope.books[0].authors).not.toBeNull();
 	}));
 
-	it('search book should fail call bookService', inject(function($q,
-			bookService, Flash) {
+	it('search book should fail call bookService', inject(function($q, bookService, Flash) {
 		// given
 		var searchDeferred = $q.defer();
 		spyOn(bookService, 'search').and.returnValue(searchDeferred.promise);
@@ -101,26 +89,24 @@ describe('book controller', function() {
 		$scope.$digest();
 		// then
 		expect(bookService.search).toHaveBeenCalled();
-		expect(Flash.create).toHaveBeenCalledWith('danger', 'Wyjątek',
-				'custom-class');
+		expect(Flash.create).toHaveBeenCalledWith('danger', 'Wyjątek', 'custom-class');
 	}));
-	
+
 	it('deleteBook is defined', inject(function() {
 		// then
 		expect($scope.deleteBook).toBeDefined();
 	}));
 
-	it('delete book should call bookService.deleteBook', inject(function($q,
-			bookService, Flash) {
+	it('delete book should call bookService.deleteBook', inject(function($q, bookService, Flash) {
 		// given
 		var bookId = 1;
 		$scope.books = [ {
-			id : bookId,
-			title : 'test'
+			id : 2, title : 'test2'
+		}, {
+			id : bookId, title : 'test'
 		} ];
 		var deleteDeferred = $q.defer();
-		spyOn(bookService, 'deleteBook').and
-				.returnValue(deleteDeferred.promise);
+		spyOn(bookService, 'deleteBook').and.returnValue(deleteDeferred.promise);
 		spyOn(Flash, 'create');
 		// when
 		$scope.deleteBook(bookId);
@@ -128,84 +114,110 @@ describe('book controller', function() {
 		$scope.$digest();
 		// then
 		expect(bookService.deleteBook).toHaveBeenCalledWith(bookId);
-		expect(Flash.create).toHaveBeenCalledWith('success',
-				'Książka została usunięta!', 'custom-class');
-		expect($scope.books.length).toBe(0);
+		expect(Flash.create).toHaveBeenCalledWith('success', 'Książka została usunięta!', 'custom-class');
+		expect($scope.books.length).toBe(1);
 	}));
 	
+	it('delete book should fail and call Flash warning', inject(function($q, bookService, Flash) {
+		// given
+		var bookId = 1;
+		$scope.books = [ {
+			id : 2, title : 'test2'
+		}, {
+			id : bookId, title : 'test'
+		} ];
+		var deleteDeferred = $q.defer();
+		spyOn(bookService, 'deleteBook').and.returnValue(deleteDeferred.promise);
+		spyOn(Flash, 'create');
+		// when
+		$scope.deleteBook(bookId);
+		deleteDeferred.reject();
+		$scope.$digest();
+		// then
+		expect(bookService.deleteBook).toHaveBeenCalledWith(bookId);
+		expect(Flash.create).toHaveBeenCalledWith('danger', 'Książka nie została usunięta!', 'custom-class');
+		expect($scope.books.length).toBe(2);
+	}));
+
 	it('addBook is defined', inject(function() {
 		// then
 		expect($scope.addBook).toBeDefined();
 	}));
 
 	it('add book should open book-modal', inject(function($modal, $q, Flash) {
-				// given
-				var modalDeferred = $q.defer();
-				var book = {
-					id : 5,
-					title : 'test'
-				};
-				var expectedParams = {
-					templateUrl : 'books/html/book-modal.html',
-					controller : 'BookModalController',
-					size : 'lg',
-					resolve : {
-						header : function() {
-							return 'Inserting new book';
-						},
-						book : function() {
-							return {id:undefined,title:'',authors:[]};
-						}
-					}
-				};
-				var spy = spyOn($modal, 'open').and.returnValue({
-					result : modalDeferred.promise
-				});
-				spyOn(Flash, 'create');
-				spyOn($scope.books, 'push');
-				// when
-				$scope.addBook();
-				modalDeferred.resolve(book);
-				$scope.$digest();
-				// then
-				expect($modal.open).toHaveBeenCalled();
-				var resultParams = spy.calls.mostRecent().args[0];
-				expect(resultParams.templateUrl).toEqual(expectedParams.templateUrl);
-				expect(resultParams.controller).toEqual(expectedParams.controller);
-				expect(resultParams.size).toEqual(expectedParams.size);
-				expect(resultParams.resolve.header()).toEqual(expectedParams.resolve.header());
-				expect(resultParams.resolve.book()).toEqual(expectedParams.resolve.book());
-				expect(Flash.create).toHaveBeenCalledWith('success',
-						'Książka "' + book.title + '" została dodana!',
-						'custom-class');
-				expect($scope.books.push).toHaveBeenCalledWith(book);
+		// given
+		var modalDeferred = $q.defer();
+		var book = {
+			id : 5, title : 'test'
+		};
+		var expectedParams = {
+			templateUrl : 'books/html/book-modal.html', controller : 'BookModalController', size : 'lg', resolve : {
+				header : function() {
+					return 'Inserting new book';
+				}, book : function() {
+					return {
+						id : undefined, title : '', authors : []
+					};
+				}
+			}
+		};
+		var spy = spyOn($modal, 'open').and.returnValue({
+			result : modalDeferred.promise
+		});
+		spyOn(Flash, 'create');
+		spyOn($scope.books, 'push');
+		// when
+		$scope.addBook();
+		modalDeferred.resolve(book);
+		$scope.$digest();
+		// then
+		expect($modal.open).toHaveBeenCalled();
+		var resultParams = spy.calls.mostRecent().args[0];
+		expect(resultParams.templateUrl).toEqual(expectedParams.templateUrl);
+		expect(resultParams.controller).toEqual(expectedParams.controller);
+		expect(resultParams.size).toEqual(expectedParams.size);
+		expect(resultParams.resolve.header()).toEqual(expectedParams.resolve.header());
+		expect(resultParams.resolve.book()).toEqual(expectedParams.resolve.book());
+		expect(Flash.create).toHaveBeenCalledWith('success', 'Książka "' + book.title + '" została dodana!',
+				'custom-class');
+		expect($scope.books.push).toHaveBeenCalledWith(book);
 	}));
-	
+
+	it('add book should fails', inject(function($modal, $q, Flash) {
+		// given
+		var modalDeferred = $q.defer();
+		spyOn($modal, 'open').and.returnValue({
+			result : modalDeferred.promise
+		});
+		spyOn(Flash, 'create');
+		// when
+		$scope.addBook();
+		modalDeferred.reject();
+		$scope.$digest();
+		// then
+		expect($modal.open).toHaveBeenCalled();
+		expect(Flash.create).toHaveBeenCalledWith('danger', 'Książka nie została dodana!', 'custom-class');
+	}));
+
 	it('editBook is defined', inject(function() {
 		// then
 		expect($scope.editBook).toBeDefined();
 	}));
-	
+
 	it('edit book should open book-modal', inject(function($modal, $q, Flash) {
 		// given
 		var modalDeferred = $q.defer();
 		var book = {
-				id : 5,
-				title : 'test'
+			id : 5, title : 'test'
 		};
 		var expectedParams = {
-				templateUrl : 'books/html/book-modal.html',
-				controller : 'BookModalController',
-				size : 'lg',
-				resolve : {
-					header : function() {
-						return 'Editing book';
-					},
-					book : function() {
-						return book;
-					}
-					
+			templateUrl : 'books/html/book-modal.html', controller : 'BookModalController', size : 'lg', resolve : {
+				header : function() {
+					return 'Editing book';
+				}, book : function() {
+					return book;
 				}
+			}
 		};
 		var spy = spyOn($modal, 'open').and.returnValue({
 			result : modalDeferred.promise
@@ -224,11 +236,28 @@ describe('book controller', function() {
 		expect(resultParams.size).toEqual(expectedParams.size);
 		expect(resultParams.resolve.header()).toEqual(expectedParams.resolve.header());
 		expect(resultParams.resolve.book()).toEqual(expectedParams.resolve.book());
-		expect(Flash.create).toHaveBeenCalledWith('success',
-				'Książka "' + book.title + '" została edytowana!',
-		'custom-class');
+		expect(Flash.create).toHaveBeenCalledWith('success', 'Książka "' + book.title + '" została edytowana!',
+				'custom-class');
 		expect($scope.books.push).toHaveBeenCalledWith(book);
 	}));
 
-	
+	it('edit book should open book-modal', inject(function($modal, $q, Flash) {
+		// given
+		var book = {
+			id : 5, title : 'test'
+		};
+		var modalDeferred = $q.defer();
+		spyOn($modal, 'open').and.returnValue({
+			result : modalDeferred.promise
+		});
+		spyOn(Flash, 'create');
+		// when
+		$scope.editBook(book);
+		modalDeferred.reject();
+		$scope.$digest();
+		// then
+		expect($modal.open).toHaveBeenCalled();
+		expect(Flash.create).toHaveBeenCalledWith('danger', 'Książka nie została edytowana!', 'custom-class');
+	}));
+
 });
